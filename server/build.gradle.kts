550c86c97043c235797e210b296b22a06971e986
Zaenal Arif <zaenalariftech@gmail.com>
2025-07-03 17:46:19 +0700

    - Migrate from EmbeddedServer to EngineMain for auto reload support - Increase JVM memory from 4GB to 6GB

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.ktorSerialization)
     application
 }
 
 group = "com.mobbelldev.boruto"
 version = "1.0.0"
 application {
-    mainClass.set("com.mobbelldev.boruto.ApplicationKt")
-    
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
--- server/src/main/kotlin/com/mobbelldev/boruto/Application.kt
+++ server/src/main/kotlin/com/mobbelldev/boruto/Application.kt
@@ -1,20 +1,25 @@
 package com.mobbelldev.boruto
 
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
-fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args = args)
 
 fun Application.module() {
     routing {
         get("/") {
             call.respondText("Ktor: ${Greeting().greet()}")
+        }
+
+        get("/heroes/") {
+            val name = call.queryParameters["name"]
+            call.respondText(
+                text = "Hello $name",
+                status = HttpStatusCode.OK
+            )
         }
     }
 }
