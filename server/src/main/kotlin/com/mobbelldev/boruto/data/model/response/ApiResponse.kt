a0fa239e475a05c05206cca507664216e5b23f95
Zaenal Arif <zaenalariftech@gmail.com>
2025-07-04 23:29:07 +0700

    update ApiResponse model to use Boolean for success field

--- server/src/main/kotlin/com/mobbelldev/boruto/data/model/response/ApiResponse.kt
+++ server/src/main/kotlin/com/mobbelldev/boruto/data/model/response/ApiResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiResponse(
-    val success: String,
+    val success: Boolean,
     val message: String? = null,
     val prevPage: Int? = null,
     val nextPage: Int? = null,
