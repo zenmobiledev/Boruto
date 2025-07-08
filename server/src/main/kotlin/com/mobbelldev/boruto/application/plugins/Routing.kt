3af46371647e3f58096da4de14c491c0db05722f
Zaenal Arif <zaenalariftech@gmail.com>
2025-07-08 12:48:49 +0700

    implement HeroRepository with hero data and pagination; update routing to include static resources

--- server/src/main/kotlin/com/mobbelldev/boruto/application/plugins/Routing.kt
+++ server/src/main/kotlin/com/mobbelldev/boruto/application/plugins/Routing.kt
@@ -3,11 +3,14 @@ package com.mobbelldev.boruto.application.plugins
 import com.mobbelldev.boruto.application.routes.getAllHeroes
 import com.mobbelldev.boruto.application.routes.root
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+
+        staticResources(remotePath = "/images", basePackage = "images")
     }
 }
--- server/src/main/kotlin/com/mobbelldev/boruto/application/routes/AllHeroes.kt
+++ server/src/main/kotlin/com/mobbelldev/boruto/application/routes/AllHeroes.kt
@@ -1,20 +1,28 @@
 package com.mobbelldev.boruto.application.routes
 
 import com.mobbelldev.boruto.data.model.response.ApiResponse
+import com.mobbelldev.boruto.domain.repository.HeroRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.application
 import io.ktor.server.routing.get
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
+    val repository: HeroRepository by application.inject()
+
     get(path = "/boruto/heroes") {
         try {
             val page: Int = call.request.queryParameters["page"]?.toInt() ?: 1
-
             require(value = page in 1..5)
+
+            val response: ApiResponse = repository.getAllHeroes(
+                page = page
+            )
             call.respond(
                 status = HttpStatusCode.OK,
-                message = page
+                message = response
             )
         } catch (_: NumberFormatException) {
             call.respond(
