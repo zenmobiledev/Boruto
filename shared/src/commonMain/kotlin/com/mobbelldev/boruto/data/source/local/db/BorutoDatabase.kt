94a19b2f2cb24d4763869348653e43098313b145
Zaenal Arif <zaenalariftech@gmail.com>
2025-07-10 23:50:23 +0700

    add TypeConverters for list-string conversion in Room database

--- shared/src/commonMain/kotlin/com/mobbelldev/boruto/data/source/local/db/BorutoDatabase.kt
+++ shared/src/commonMain/kotlin/com/mobbelldev/boruto/data/source/local/db/BorutoDatabase.kt
@@ -2,12 +2,15 @@ package com.mobbelldev.boruto.data.source.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.mobbelldev.boruto.data.source.local.DatabaseConverter
 import com.mobbelldev.boruto.data.source.local.dao.HeroDao
 import com.mobbelldev.boruto.data.source.local.dao.HeroRemoteKeyDao
 import com.mobbelldev.boruto.data.source.local.entity.HeroEntity
 import com.mobbelldev.boruto.data.source.local.entity.HeroRemoteKeyEntity
 
 @Database(entities = [HeroEntity::class, HeroRemoteKeyEntity::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase : RoomDatabase() {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
